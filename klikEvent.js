7d8651cc4ac3d61b2cdd41d0afb74e795856e6e8
Matthieu Harbich <matthieu.harbich@gmail.com>
2019-05-02 01:45:49 +0200

    init

--- klikEvent.js
+++ klikEvent.js
@@ -1,24 +1,11 @@
 KlikLayer.prototype.sideBox = function(){
-	console.log(this);
 	this.layerEventInit.push({
 		sideBox: this.id
 	})
-
-}
-
-KlikLayer.prototype.boxEvent = function(){
-	console.log(this);
-	this.layerEventInit.push({
-		boxEvent: this.id
-	})
-
 }
 
 KlikPath.prototype.sideBox = function(){
-	
-	console.log(this.layersId)
 	var id = this.layersId[0].sideBox;
-	console.log(id)
 	$('#'+ id).empty();
 	$('#'+ id).append('<button style="position:absolute; top: 10px; right: 10px;" id="closeBox"> X </button> <h1 style="text-align: center;">'+ this.polygon.element.name+'</h1>');
 	
@@ -29,13 +16,8 @@ KlikPath.prototype.sideBox = function(){
 		$('#'+ id).fadeOut( 200, function() {
 
 		  });
-	})
-	
+	})	
 	
 }
 
 
-KlikPath.prototype.boxEvent = function(){
-	var id = this.layersId[0].boxEvent;
-
-}
